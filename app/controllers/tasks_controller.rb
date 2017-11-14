704221ed54017bfc5e3be8968dd948ac714038fa
Ryan Kretch <rkretch91@gmail.com>
2017-11-14 16:25:33 +0800

    update function has been fixed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
@@ -23,7 +24,7 @@ class TasksController < ApplicationController
   end
 
   def update
-    @task = Task.update(task_params)
+    @task.update(task_params)
     redirect_to tasks_path
   end
 
@@ -33,12 +34,14 @@ class TasksController < ApplicationController
   end
 
   private
-  def task_params
-    params.require(:task).permit(:name, :details)
-  end
 
   def set_task
     @task = Task.find(params[:id])
   end
+
+  def task_params
+    params.require(:task).permit(:name, :details)
+  end
+
 
 end
